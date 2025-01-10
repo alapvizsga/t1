afc9c8854de0c0a5d38e90297fffc2a48a692c64
Englert Ervin  Brunó <englert@users.noreply.github.com>
2025-01-10 05:05:44 +0100

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,33 @@
+def osszead(szam1, szam2):
+    """ A függvény két számot kap és 
+            visszatér a két szám összegével.
+    """
+    pass # Írd a kódodat a következő sorokba!
+
+# Ellenőrizd a függvényt a cella futtatásával! 
+# Az assert csak hiba esetén ad visszajelzést.
+#assert osszead(14, -8) == 6
+
+def kisebb(szam1, szam2):
+    """ A függvény két számot kap és 
+            visszatér a kisebbel.
+    """
+    pass # Írd a kódodat a következő sorokba!
+    
+# Ellenőrizd a függvényt a cella futtatásával! 
+# Az assert csak hiba esetén ad visszajelzést.
+
+#assert kisebb(10, -7) == -7
+#assert kisebb(-10, 7) == -10
+
+def nagyobb(szam1, szam2):
+    """ A függvény két számot kap és 
+            visszatér a nagyobbal.
+    """
+    pass # Írd a kódodat a következő sorokba!
+
+# Ellenőrizd a függvényt a cella futtatásával! 
+# Az assert csak hiba esetén ad visszajelzést.
+
+#assert nagyobb(12, -8) == 12
+#assert nagyobb(-12, -8) == -8
